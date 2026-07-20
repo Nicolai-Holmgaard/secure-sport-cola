17e4dd5faf49cdac231ed0077b30970d0c7f63e6
Nicolai Holmgaard <Nicolai@Holmgaard.info>
2026-07-20 11:19:05 +0200

    Added error handeling

--- src/main.rs
+++ src/main.rs
@@ -17,7 +17,7 @@ async fn main() -> Result<(), confy::ConfyError> {
     let mut cfg: cli::SSCConfig = confy::load("secure-sport-cola", config_name.as_str())?;
 
     let username = cli.username.unwrap_or(cfg.username.clone());
-    let room = cli.room.unwrap_or(cfg.room.clone());
+    let room = cli.room.unwrap_or(cfg.room);
     if cfg.username.is_empty() {
         if username.is_empty() {
             eprintln!(
@@ -33,19 +33,51 @@ async fn main() -> Result<(), confy::ConfyError> {
         eprintln!("URL is not defined in the config file.");
         std::process::exit(1);
     }
-    let member_id = get_member_id(&cfg.url, &username).await.unwrap();
+    let member_id = match get_member_id(&cfg.url, &username).await {
+        Ok(mem_id) => mem_id,
+        Err(err) => {
+            if err.to_string() == "error decoding response body" {
+                eprintln!(
+                    "You do not have access to the stregsystem, try changing networks or starting a VPN."
+                );
+                std::process::exit(1);
+            } else if err
+                .to_string()
+                .starts_with("error sending request for url (http")
+            {
+                eprintln!("Couldn't send reqest. Possibly missing internet connection.");
+                std::process::exit(1);
+            }
+            panic!("{}", err);
+        }
+    };
 
     if cli.balance {
-        let balance = get_member_balance(&cfg.url, &member_id).await.unwrap();
+        let balance = match get_member_balance(&cfg.url, &member_id).await {
+            Ok(bal) => bal,
+            Err(err) => panic!("{}", err),
+        };
         println!("Balance: {}", (balance as f32) / 100.0);
         return Ok(());
     }
     if cli.list {
-        let products = get_active_products(&cfg.url, room).await.unwrap();
-        let named_products = get_named_products(&cfg.url).await.unwrap();
+        let products = match get_active_products(&cfg.url, room).await {
+            Ok(products) => products,
+            Err(err) => {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        };
+        let named_products = match get_named_products(&cfg.url).await {
+            Ok(named_products) => named_products,
+            Err(err) => {
+                eprintln!("{}", err);
+                std::process::exit(1);
+            }
+        };
         println!("Active products:");
         for (id, product) in products {
-            let short_id: i32 = id.parse().unwrap();
+            let short_id: i32 = id.parse().expect("Couldn't convert to integer");
             let shorts: Vec<String> = named_products
                 .clone()
                 .into_iter()
@@ -73,14 +105,27 @@ async fn main() -> Result<(), confy::ConfyError> {
         return Ok(());
     }
 
+    if cli.buystring.is_empty() {
+        eprintln!("No buy string given, doing nothing.");
+        return Ok(());
+    }
+
     let sale_req = SaleRequest {
         member_id,
-        room: room,
+        room,
         buystring: format!("{} {}", &username, cli.buystring.join(" ")),
     };
 
     println!("{:?}", sale_req);
 
-    post_sale(&cfg.url, sale_req).await.unwrap();
+    match post_sale(&cfg.url, sale_req).await {
+        Ok(_) => {}
+        Err(err) => {
+            println!(
+                "Error happend during sale, please check with the official stregsystem wether you have bought your items. \n Error: {}",
+                err
+            );
+        }
+    }
     Ok(())
 }
